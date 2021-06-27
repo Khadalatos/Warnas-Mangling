cce2493ee5e59f4c24608f5290e21942f06cb845
Khadalatos <81304535+Khadalatos@users.noreply.github.com>
2021-06-27 18:25:22 +0700

    Add files via upload

---
+++ paket/page3.php
@@ -0,0 +1,263 @@
+<?php include('../admin/partials/menu1.php'); ?>
+<!DOCTYPE php>
+<html>
+<head>
+	<meta">
+	<title>Warung Sunda | Paket</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="judul1.png">
+</head>
+<body>
+	<div id="home">
+	<header id="home">
+		<a href="#home" class="logo">
+			<img src="logo1.png" width="200"></a>
+      <ul>
+			<li><a href="<?php echo SITEURL;  ?>home/"><b>Beranda</b></li></a>
+			<li><a href="<?php echo SITEURL;  ?>menu/page2.php"><b>Menu</b></li></a>
+			<li><a href="<?php echo SITEURL;  ?>paket/page3.php"><b>Paket</b></li></a>
+			<li><a href="<?php echo SITEURL;  ?>home/index.php #about"><b>Tentang</b></li></a>
+			<li><a href="<?php echo SITEURL;  ?>home/index.php #contact"><b>Hubungi</b></li>	</a>
+		</ul>
+	</header>
+</div>
+
+<div class="bg-cl" > <br><br><br><br><br>
+
+	<h3 id="kadalbunting"><b>Silahkan Lihat Paket yang Kami Miliki!</b></h3>
+
+	<h1 id="kadalbunting"><b>Paket Warnas Mangling</b></h1>
+	<h2 id="kadalbunting">Paketnya Murah dan Ramah Dompet</h2>
+
+<br><br><br><br><br><br>
+
+</div>
+	<div class="gtco-section">
+    <div class="gtco-container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+          <h2 class="cursive-font primary-color">Menu Paket</h2>
+          <p>Menu Paket kami bisa dibilang sangatlah murah dan juga terjangkau bagi anda.</p>
+          <h4>Jika ingin menambahkan makanan lain seperti gorengan/menu lainnya, cantumkan saja saat memesan.</h4>
+        </div>
+      </div>
+      <div class="j1">
+      <h1>Paket 1 Porsi</h1><Br>
+      </div>
+      <div class="row">
+
+        <div class="col-lg-4 col-md-4 col-sm-6">
+          <a href="images/img_1.jpg" class="fh5co-card-item image-popup">
+            <figure>
+              <div class="overlay"><i class="ti-plus"></i></div>
+              <img src="images/img_1.jpg" alt="Image" class="img-responsive">
+            </figure>
+            <div class="fh5co-text">
+              <h2><b>Paket Liwet 1 Porsi</b></h2>
+              <p> 1 Nasi Putih/Liwet <br>
+                  1 Ayam Goreng (Paha/Sayap)<br>
+                  2 Tempe Goreng<br>
+                  1 Tahu Goreng<br>
+                  Kerupuk<br>
+                  Lalapan<br>
+                  Sambal (Terasi/Matah) <br>
+                  1 Teh Hangat (Manis/Tawar) </p><br><br><br><br><br>
+              <p><span class="price cursive-font">Rp.20.000</span></p>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6">
+          <a href="images/img_2.jpg" class="fh5co-card-item image-popup">
+            <figure>
+              <div class="overlay"><i class="ti-plus"></i></div>
+              <img src="images/img_2.jpg" alt="Image" class="img-responsive">
+            </figure>
+            <div class="fh5co-text">
+              <h2><b>Paket Lengkap 1 Porsi Mantap </b></h2>
+              <p> 1 Nasi Putih/Liwet <br>
+                  1 Ayam Goreng (Paha/Sayap/Dada)<br>
+                  2 Tempe Goreng<br>
+                  2 Tahu Goreng<br>
+                  1 Ikan Asin<br>
+                  2 Cumi Goreng <br>
+                  Kerupuk<br>
+                  Lalapan<br>
+                  Sambal (Terasi/Matah)<br>
+                  1 Teh Hangat (Manis/Tawar) </p><br><br><br>
+              <p><span class="price cursive-font">Rp.27.000</span></p>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6">
+          <a href="images/img_3.jpg" class="fh5co-card-item image-popup">
+            <figure>
+              <div class="overlay"><i class="ti-plus"></i></div>
+              <img src="images/img_3.jpg" alt="Image" class="img-responsive">
+            </figure>
+            <div class="fh5co-text">
+              <h2><b>Paket Lengkap 1 Porsi Luar Biasa </b></h2>
+              <p> 1 Nasi Putih/Liwet <br>
+                  1 Ayam Goreng (Paha/Sayap/Dada)<br>
+                  3 Tempe Goreng<br>
+                  2 Tahu Goreng<br>
+                  1 Ikan Asin<br>
+                  2 Cumi Goreng <br>
+                  1 Sayur Asem<br>
+                  Karedok Porsi Kecil<Br>
+                  Kerupuk<br>
+                  Lalapan<br>
+                  Sambal (Terasi/Matah)<br>
+                  1 Teh Hangat (Manis/Tawar) </p>
+              <p><span class="price cursive-font">Rp.35.000</span></p>
+
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+        <div class="j1">
+      <h1>Paket 5 Porsi</h1>
+      </div>
+  <div class="gtco-section">
+    <div class="gtco-container">
+      <div class="row">
+  <div class="col-lg-4 col-md-4 col-sm-6">
+          <a href="images/img_4.jpg" class="fh5co-card-item image-popup">
+            <figure>
+              <div class="overlay"><i class="ti-plus"></i></div>
+              <img src="images/img_4.jpg" alt="Image" class="img-responsive">
+            </figure>
+            <div class="fh5co-text">
+              <h2><b>Paket Lengkap 5 Porsi</b></h2>
+              <p> 5 Nasi Putih <br>
+                  5 Ayam Goreng (Paha/Sayap)<br>
+                  10 Tempe Goreng<br>
+                  6 Tahu Goreng<br>
+                  5 Mie Goreng/Bihun Goreng<br>
+                  Kerupuk<br>
+                  Lalapan<br>
+                  Sambal (Terasi/Matah)<br>
+                  1 Teh Hangat (Manis/Tawar) </p><br><br><br><br><br>
+              <p><span class="price cursive-font">Rp.110.000</span></p>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6">
+          <a href="images/img_5.jpg" class="fh5co-card-item image-popup">
+            <figure>
+              <div class="overlay"><i class="ti-plus"></i></div>
+              <img src="images/img_5.jpg" alt="Image" class="img-responsive">
+            </figure>
+            <div class="fh5co-text">
+              <h2><b>Paket Lengkap 5 Porsi Mantap</b></h2>
+              <p> 5 Nasi Putih/Liwet <br>
+                  5 Ayam Goreng (Paha/Sayap/Dada)<br>
+                  10 Tempe Goreng<br>
+                  7 Tahu Goreng<br>
+                  5 Ikan Asin<br>
+                  10 Cumi Goreng <br>
+                  5 Mie Goreng/Bihun Goreng<br>
+                  Kerupuk<br>
+                  Lalapan<br>
+                  Sambal (Terasi/Matah)<br>
+                  1 Teh Hangat (Manis/Tawar) </p><br><br><br>
+              <p><span class="price cursive-font">Rp.125.000</span></p>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6">
+          <a href="images/img_6.jpg" class="fh5co-card-item image-popup">
+            <figure>
+              <div class="overlay"><i class="ti-plus"></i></div>
+              <img src="images/img_6.jpg" alt="Image" class="img-responsive">
+            </figure>
+            <div class="fh5co-text">
+              <h2><b>Paket Lengkap 5 Porsi Luar Biasa</b></h2>
+              <p> 5 Nasi Putih/Liwet <br>
+                  5 Ayam Goreng (Paha/Sayap/Dada)<br>
+                  12 Tempe Goreng<br>
+                  10 Tahu Goreng<br>
+                  5 Ikan Asin<br>
+                  10 Cumi Goreng <br>
+                  10 Sayur Asem<br>
+                  5 Mie Goreng/Bihun Goreng<br>
+                  5 Karedok Porsi Kecil<Br>
+                  Kerupuk<br>
+                  Lalapan<br>
+                  Sambal (Terasi/Matah)<br>
+                  1 Teh Hangat (Manis/Tawar) </p>
+              <p><span class="price cursive-font">Rp.170.000</span></p>
+            </div>
+          </a>
+        </div>
+
+     </div>
+    </div>
+  </div>
+	    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>Cerita Warnas Mangling</h6>
+            <p class="text-justify">Warnas Mangling dulunya sangatlah sederhana, hanya restoran yang berukuran sangat kecil, kami membuka restoran ini awalnya pada tahun 2002 yang awalnya bernama "Rumah Makan Kang Ling" tetapi diubah menjadi "Warnas Mangling" karena lebih mudah dipanggil.</p>
+          </div>
+
+          
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Hari Buka </h6>
+            <ul class="footer-links">
+              <li><a href="#home">Senin : 09:00 - 21:00</a></li>
+              <li><a href="#porto">Selasa : 09:00 - 21:00</a></li>
+              <li><a href="#resume">Rabu : 09:00 - 21:00</a></li>
+              <li><a href="#about">Kamis : 09:00 - 21:00</a></li>
+              <li><a href="#contact">Jum'at : 13:00 - 22:00</a></li>
+              <li><a href="#contact">Sabtu : Libur</a></li>
+              <li><a href="#contact">Minggu : 10:00 - 22:00</a></li>
+            </ul>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <h6>Link Cepat</h6>
+            <ul class="footer-links">
+              <li><a href="<?php echo SITEURL;  ?>home/">Beranda</li></a>
+              <li><a href="<?php echo SITEURL;  ?>menu/page2.php">Menu</li></a>
+			        <li><a href="<?php echo SITEURL;  ?>paket/page3.php">Paket</li></a>
+			        <li><a href="<?php echo SITEURL;  ?>home/index.php #about">Tentang</li></a>
+			        <li><a href="<?php echo SITEURL;  ?>home/index.php #contact">Hubungi</li>	</a>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+
+      <div class="container d-flex justify-content-between">
+        <div class="f1">
+            <h1 class="">© 2021 - warnasmangling.com - all right reserved</h1>
+            <h1 class="">Design by Lingmation Studio</h1>
+        </div>
+        
+        
+      </div>
+</footer>
+	<script type="text/javascript">
+		window.addEventListener("scroll", function(){
+			var header = document.querySelector("header");
+			header.classList.toggle("sticky", window.scrollY > 0)
+		})
+	</script>
+
+
+
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" integrity="sha512-DkPsH9LzNzZaZjCszwKrooKwgjArJDiEjA5tTgr3YX4E6TYv93ICS8T41yFHJnnSmGpnf0Mvb5NhScYbwvhn2w==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js" integrity="sha512-0xrMWUXzEAc+VY7k48pWd5YT6ig03p4KARKxs4Bqxb9atrcn2fV41fWs+YXTKb8lD2sbPAmZMjKENiyzM/Gagw==" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
